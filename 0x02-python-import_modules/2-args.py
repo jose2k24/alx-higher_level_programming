1e30b6e95ec63e68d80b77ec101c85b6b593c365
jose2k24 <josi.endal45@gmail.com>
2022-06-08 13:40:40 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     argc = len(argv) - 1
     if argc == 0:
         print("0 arguments.")
-    else:
-        if argc == 1:
+    elif:
+         argc == 1:
             print("1 argument:")
         else:
             print("{} arguments:".format(argc))
