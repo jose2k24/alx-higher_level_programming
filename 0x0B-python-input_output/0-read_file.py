40065528cffcbc2375cf472a68c3d4cbc88f21d2
Joseph Endale <josi.endal45@gmail.com>
2022-07-05 23:12:38 +0300

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r" , encoding = "utf8" ) as f:
-        print(f.read(), end="")
+    with open(filename,"r") as f:
+        filename = f.read()
+        print(filename,end="")
