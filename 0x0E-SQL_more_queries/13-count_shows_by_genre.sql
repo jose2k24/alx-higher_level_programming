e822ad20fe9326ec98cfc8d6f604fe29e1242242
Joseph Endale <josi.endal45@gmail.com>
2022-08-10 20:20:03 +0300

    SQL - More queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,8 +8,3 @@ FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_shows DESC;
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
