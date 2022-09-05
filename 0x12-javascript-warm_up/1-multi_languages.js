68d1bd9918d00654824f1722532615782b8db3fa
Joseph Endale <josi.endal45@gmail.com>
2022-09-05 22:45:08 +0300

    JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,12 +3,10 @@
 var str = "C is fun";
 console.log(str);
 </script>
-
 <script>
 var str = "Python is cool";
 console.log(str);
 </script>
-
 <script>
 var str = "Javascript is amazing";
 console.log(str);
